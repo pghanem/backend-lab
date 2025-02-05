2f30a5720f8f63159facaad8db2046fdba35e0b2
Peter Ghanem <peterjonathang@gmail.com>
2025-02-04 20:48:20 -0800

    [fix] remove dotenv and use hardcoded port for now

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
 
-dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
 	res.send('Backend Sandbox API is running');

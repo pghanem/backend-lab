3d87aaa90ee3991bcbcbc4fc9defff18f9ea3603
Peter Ghanem <peterjonathang@gmail.com>
2025-02-05 20:00:21 -0800

    [fix] changing some labelling to help clarity

--- src/pocs/http/rest-api.ts
+++ src/pocs/http/rest-api.ts
@@ -5,7 +5,7 @@ const router = Router();
 // Example route for the simple REST API
 router.get('/restGet', (req: Request, res: Response) => {
 	res.status(200).json({
-		message: 'This is your first REST API get request!',
+		message: 'Successful GET request!',
 	});
 });
 

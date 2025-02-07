603335f3a681b5d989b6b7c42de0fca7c0e98b9e
Peter Ghanem <peterjonathang@gmail.com>
2025-02-06 22:40:39 -0800

    [feature] adding other request type and trigger buttons

--- src/pocs/http/rest-api.ts
+++ src/pocs/http/rest-api.ts
@@ -2,11 +2,31 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-// Example route for the simple REST API
+// Example routes for a simple REST API
 router.get('/restGet', (req: Request, res: Response) => {
 	res.status(200).json({
 		message: 'Successful GET request!',
 	});
 });
+
+router.post('/restPost', (req: Request, res: Response) => {
+	res.status(200).json({
+		message: 'Successful POST request!',
+	});
+});
+
+router.put('/restPut', (req: Request, res: Response) => {
+	res.status(200).json({
+		message: 'Successful PUT request!',
+	});
+});
+
+router.delete('/restDelete', (req: Request, res: Response) => {
+	res.status(200).json({
+		message: 'Successful DELETE request!',
+	});
+});
+
+
 
 export default router;

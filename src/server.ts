d26e18a32ec854840e7a4b37753cffd03e3fa6a1
Peter Ghanem <peterjonathang@gmail.com>
2025-02-11 22:20:35 -0800

    [chore] remove comments

--- src/server.ts
+++ src/server.ts
@@ -4,27 +4,21 @@ import restApi from './pocs/http/rest-api';
 import dotenv from 'dotenv';
 import db from "./config/db";
 
-// Load the .env file
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON bodies of incoming requests
 app.use(express.json());
 
-// Mount the REST API POC at '/api/restApi'
-app.use('/restApi', restApi);  // This means the REST POC route will be accessible under '/api/restApi'
+app.use('/restApi', restApi);
 
-// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Default route to serve the index.html
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/index.html'));  // Serve the front-end HTML file
+	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Simple GET route to check the database connection
 app.get('/api/test-db', (req: Request, res: Response) => {
 	db.query('SELECT NOW()', (err, results) => {
 		if (err) {
@@ -35,7 +29,6 @@ app.get('/api/test-db', (req: Request, res: Response) => {
 	});
 });
 
-// Start the server and listen on the defined port
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
